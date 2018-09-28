f00227bab5da906e48c9255642909ca5391438af
lyrieek <tianhao7@foxmail.com>
2018-09-28 22:38:21 +0800

    init docs-tools config

--- server.js
+++ server.js
@@ -1,6 +1,44 @@
 const liveServer = require('live-server')
+const rollup = require('rollup')
+const buble = require('rollup-plugin-buble')
+const commonjs = require('rollup-plugin-commonjs')
+const nodeResolve = require('rollup-plugin-node-resolve')
+const uglify = require('rollup-plugin-uglify')
+const replace = require('rollup-plugin-replace')
 
-liveServer.start({
-  port: 393,
-  watch: ['lib', 'docs', 'themes']
-})
+const opts = {
+	input: 'src/index.js',
+	output: 'docs-tool.js',
+	version: '1.1.0'
+}
+
+rollup.rollup({
+		input: opts.input,
+		plugins: (opts.plugins || []).concat([
+			buble(),
+			commonjs(),
+			nodeResolve(),
+			replace({
+				__VERSION__: opts.version,
+				'process.env.SSR': false
+			})
+		])
+	})
+	.then(function(bundle) {
+		var dest = 'lib/' + (opts.output || opts.input)
+
+		console.log(dest)
+		bundle.write({
+			format: 'iife',
+			file: dest,
+			strict: false
+		})
+	})
+	.catch(function(err) {
+		console.error(err)
+	})
+
+// liveServer.start({
+// 	port: 393,
+// 	watch: ['lib', 'docs', 'themes']
+// });
